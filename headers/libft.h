1d03711c7059723daa81470e7450d5b11c5a9d4b
Memthos <163067951+Memthos@users.noreply.github.com>
2026-01-24 02:07:43 +0100

    Remove unused struct

--- headers/libft.h
+++ headers/libft.h
@@ -17,12 +17,6 @@
 # include <unistd.h>
 # include "../libs/ft_printf/headers/ft_printf.h"
 
-typedef struct s_list
-{
-	void			*content;
-	struct s_list	*next;
-}	t_list;
-
 int		ft_isalpha(int c);
 int		ft_isdigit(int c);
 int		ft_isalnum(int c);
