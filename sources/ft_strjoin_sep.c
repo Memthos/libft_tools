91c71d3234ab33ee324ecd9ca20e222937e9fb26
Memthos <163067951+Memthos@users.noreply.github.com>
2026-02-07 19:53:27 +0100

    Add ft_strjoin_sep function implementation

---
+++ sources/ft_strjoin_sep.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin_sep.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mperrine <mperrine@student.42angouleme.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/07 13:22:46 by mperrine          #+#    #+#             */
+/*   Updated: 2026/02/07 10:18:10 by mperrine         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strjoin_sep(const char *s1, const char *s2, char sep)
+{
+	size_t	i;
+	size_t	j;
+	char	*str;
+
+	if (!s1 || !s2 || sep == 0)
+		return (NULL);
+	str = malloc(sizeof(char) + (ft_strlen(s1) + ft_strlen(s2) + 2));
+	if (!str)
+		return (NULL);
+	i = 0;
+	while (s1[i])
+	{
+		str[i] = s1[i];
+		i++;
+	}
+	str[i++] = sep;
+	j = 0;
+	while (s2[j])
+		str[i++] = s2[j++];
+	str[i] = '\0';
+	return (str);
+}
