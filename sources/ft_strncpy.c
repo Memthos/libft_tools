92f8248f1a56c3fe391c60db50431e983bc2636d
Memthos <perrinetrebi.m@gmail.com>
2026-03-08 00:59:35 +0100

    changed strcpy to strncpy

--- sources/ft_strncpy.c
+++ sources/ft_strncpy.c
@@ -1,30 +1,33 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strcpy.c                                        :+:      :+:    :+:   */
+/*   ft_strncpy.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mperrine <mperrine@student.42angouleme.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/14 10:09:30 by mperrine          #+#    #+#             */
-/*   Updated: 2026/03/07 23:57:34 by mperrine         ###   ########.fr       */
+/*   Updated: 2026/03/08 00:58:02 by mperrine         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/libft.h"
 
-size_t	ft_strcpy(char *dest, const char *src, size_t size)
+char	*ft_strncpy(char *dest, const char *src, size_t size)
 {
 	size_t	i;
-	size_t	srclen;
 
-	srclen = ft_strlen(src);
 	if (size < 1)
-		return (srclen);
+		return (dest);
 	i = 0;
-	while (i < size)
+	while (i < size && src[i])
 	{
 		dest[i] = src[i];
 		i++;
 	}
-	return (srclen);
+	while (i < size)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+	return (dest);
 }
