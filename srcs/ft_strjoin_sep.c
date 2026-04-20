0c36bae54c2580a40bab4fd6ca286d99356057a4
Mathis Perrinet-Rebibo <163067951+Memthos@users.noreply.github.com>
2026-04-20 20:43:03 +0200

    Fix null checks for string join function

--- srcs/ft_strjoin_sep.c
+++ srcs/ft_strjoin_sep.c
@@ -18,20 +18,20 @@ char	*ft_strjoin_sep(const char *s1, const char *s2, char sep)
 	size_t	j;
 	char	*str;
 
-	if (!s1 || !s2 || sep == 0)
+	if ((!s1 && !s2) || sep == 0)
 		return (NULL);
 	str = malloc(sizeof(char) + (ft_strlen(s1) + ft_strlen(s2) + 2));
 	if (!str)
 		return (NULL);
 	i = 0;
-	while (s1[i])
+	while (s1 && s1[i])
 	{
 		str[i] = s1[i];
 		i++;
 	}
 	str[i++] = sep;
 	j = 0;
-	while (s2[j])
+	while (s2 && s2[j])
 		str[i++] = s2[j++];
 	str[i] = '\0';
 	return (str);
